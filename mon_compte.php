56499e9f3d15fe35dabd20aece316d351e6d63e2
shbibsamy <shbibsamy@gmail.com>
2022-07-05 16:12:44 +0200

    petite corrections

--- mon_compte.php
+++ mon_compte.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/styles.css">
-    <title>Bienvenue</title>
+    <title>Compte</title>
 </head>
 <body>
     <div class="wrapper">
         <header>
-            <h1>Accueil Utilisateur</h1>
+            <h1>Compte</h1>
             <?php require 'menu.php'; ?>
         </header>
         <main>
